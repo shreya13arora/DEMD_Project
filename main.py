3436f238fabfda0c5c6b2c18cdf60df4118c54e0
Shreya Arora <72308010+shreya13arora@users.noreply.github.com>
2021-05-13 10:16:29 +0530

    modified main.py

--- main.py
+++ main.py
@@ -42,5 +42,4 @@ def predict_rating(data:RecommendationSystem):
 
 
 if __name__=="__main__":
-    port = int(os.environ.get("PORT",8000))
-    uvicorn.run(app, host='127.0.0.1', port=port)
+    uvicorn.run(app, host='0.0.0.0', port=5000)
